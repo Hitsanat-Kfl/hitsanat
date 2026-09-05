341656206084b79410da8846c192713a81e39107
abishabr <abrhamhabtamu52@gmail.com>
2026-09-05 15:13:43 +0300

    feat(family): add validation schemas

--- packages/validation/src/index.ts
+++ packages/validation/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./common.js";
 export * from "./member.js";
+export * from "./family.js";
 export * from "./child.js";
 export * from "./planning.js";
 export * from "./attendance.js";
